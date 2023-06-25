248806e35ceb554f8faaeb0b22242841a14c497c
natalijakominac <natalija.kominac@gmail.com>
2023-06-25 22:37:05 +0200

    Added table of loaded users with possibilities to toggle user's activity and to open dialog for adding new users; Set initial users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Observable, Subscription } from 'rxjs';
+
+import { User } from './shared/models/user';
+import { UsersQuery } from './shared/state/users.query';
+import { UsersService } from './shared/state/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = '4create-angular-project';
+export class AppComponent implements OnInit, OnDestroy {
+  users$: Observable<User[]> = this.usersQuery.selectAll();
+
+  columnsData = [
+    { key: 'id', label: 'ID' },
+    { key: 'name', label: 'Name' },
+    { key: 'active', label: 'Active' },
+  ];
+  data: User[] = [];
+
+  private subscriptions: Subscription[] = [];
+
+  get columns(): string[] {
+    return this.columnsData.map((column) => column.key);
+  }
+
+  constructor(
+    private usersQuery: UsersQuery,
+    private usersService: UsersService
+  ) {
+    this.usersService.setInitialUsers([
+      { id: 1, name: 'Pera', active: true },
+      { id: 2, name: 'Mika', active: false },
+      { id: 3, name: 'Zika', active: true },
+    ]);
+  }
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.users$.subscribe((users) => {
+        this.data = users;
+      })!
+    );
+  }
+
+  toggleUserActive(user: User): void {}
+
+  openAddUserDialog(): void {}
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
+import { UsersService } from './shared/state/users.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatButtonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [UsersService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
