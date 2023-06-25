e007afc0df733c6dddee0e668826401a3c68ada3
natalijakominac <natalija.kominac@gmail.com>
2023-06-25 22:47:19 +0200

    Implemented adding new user functionality

--- src/app/shared/dialogs/add-user-dialog/add-user-dialog.component.ts
+++ src/app/shared/dialogs/add-user-dialog/add-user-dialog.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+
+import { first, Observable, Subscription } from 'rxjs';
+
+import { User } from '../../models/user';
+import { UsersQuery } from '../../state/users.query';
+import { UsersService } from '../../state/users.service';
 
 @Component({
   selector: 'app-add-user-dialog',
   templateUrl: './add-user-dialog.component.html',
   styleUrls: ['./add-user-dialog.component.scss'],
 })
-export class AddUserDialogComponent {
+export class AddUserDialogComponent implements OnDestroy {
+  nextId$?: Observable<number> = this.usersQuery.getNextId();
+
   activeOptions: { id: boolean; label: string }[] = [
     { id: true, label: 'Active' },
     { id: false, label: 'Inactive' },
@@ -18,17 +26,37 @@ export class AddUserDialogComponent {
     active: new FormControl(this.activeOptions[1].id, Validators.required),
   });
 
+  private subscriptions: Subscription[] = [];
+
   get formValue() {
     return this.userForm.value;
   }
 
   constructor(
     public dialogRef: MatDialogRef<AddUserDialogComponent>,
+    private usersService: UsersService,
+    private usersQuery: UsersQuery,
   ) {}
 
   close(): void {
     this.dialogRef.close();
   }
 
-  submit(): void { }
+  submit(): void {
+    this.subscriptions.push(
+      this.nextId$?.pipe(first()).subscribe((nextId: number) => {
+        const user: User = {
+          id: nextId,
+          name: this.formValue.name!,
+          active: this.formValue.active!,
+        };
+        this.usersService.addUser(user);
+        this.dialogRef.close();
+      })!
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
 }
--- src/app/shared/state/users.query.ts
+++ src/app/shared/state/users.query.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 
 import { QueryEntity } from '@datorama/akita';
 
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { UsersState, UsersStore } from './users.store';
 
 @Injectable({
@@ -10,5 +13,11 @@ import { UsersState, UsersStore } from './users.store';
 export class UsersQuery extends QueryEntity<UsersState> {
   constructor(protected override store: UsersStore) {
     super(store);
+  }
+
+  getNextId(): Observable<number> {
+    return this.selectAll().pipe(
+      map((users) => Math.max(...users.map((user) => user.id)) + 1)
+    );
   }
 }
